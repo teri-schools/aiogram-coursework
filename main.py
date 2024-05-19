da574147467c29c801c84a0354b95b918d9459b9
teri-anric <2005ahi2005@gmail.com>
2024-05-19 21:36:19 +0300

    fix not import F

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import sys
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
